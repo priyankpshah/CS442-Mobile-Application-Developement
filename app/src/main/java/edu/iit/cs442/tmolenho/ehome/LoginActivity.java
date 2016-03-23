f4b45d71eb7e2f6a68db48c2a1da818b610f7b69
Tom Molenhouse <tmolenho@hawk.iit.edu>
2016-03-23 17:27:36 -0500

    added simple login button

--- app/src/main/java/edu/iit/cs442/tmolenho/ehome/LoginActivity.java
+++ app/src/main/java/edu/iit/cs442/tmolenho/ehome/LoginActivity.java
@@ -1,13 +1,30 @@
 package edu.iit.cs442.tmolenho.ehome;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.loginButton:
+                Intent login = new Intent(this, MainActivity.class);
+                startActivity(login);
+                break;
+        }
+    }
+
 }
