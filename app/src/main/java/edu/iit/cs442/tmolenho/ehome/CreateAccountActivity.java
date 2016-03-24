027dc9d9f31e68df713f14d0122a065ae2a0a5fe
Tom Molenhouse <tmolenho@hawk.iit.edu>
2016-03-24 16:40:06 -0500

    added login screen from mockup

---
+++ app/src/main/java/edu/iit/cs442/tmolenho/ehome/CreateAccountActivity.java
@@ -0,0 +1,14 @@
+package edu.iit.cs442.tmolenho.ehome;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+
+}
--- app/src/main/java/edu/iit/cs442/tmolenho/ehome/LoginActivity.java
+++ app/src/main/java/edu/iit/cs442/tmolenho/ehome/LoginActivity.java
@@ -15,14 +15,22 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         Button loginButton = (Button) findViewById(R.id.loginButton);
         loginButton.setOnClickListener(this);
+
+        Button registerButton = (Button) findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.loginButton:
+                // TODO verify login info
                 Intent login = new Intent(this, MainActivity.class);
                 startActivity(login);
+                break;
+            case R.id.registerButton:
+                Intent register = new Intent(this, CreateAccountActivity.class);
+                startActivity(register);
                 break;
         }
     }
