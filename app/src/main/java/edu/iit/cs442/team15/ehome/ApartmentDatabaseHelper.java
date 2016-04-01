d086eef649d00788adb4ac7b69f297c3f77b5594
Tom Molenhouse <tmolenho@hawk.iit.edu>
2016-03-31 23:57:34 -0500

    copy db from assets on first use and check login info

--- app/src/main/java/edu/iit/cs442/team15/ehome/ApartmentDatabaseHelper.java
+++ app/src/main/java/edu/iit/cs442/team15/ehome/ApartmentDatabaseHelper.java
@@ -3,7 +3,6 @@ package edu.iit.cs442.team15.ehome;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.util.Log;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -17,11 +16,13 @@ public class ApartmentDatabaseHelper extends SQLiteOpenHelper {
 
     private static ApartmentDatabaseHelper sInstance; // singleton instance
 
-    private Context context;
+    private final Context context;
 
     private ApartmentDatabaseHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
         this.context = context;
+
+        setupDatabase();
     }
 
     public static synchronized ApartmentDatabaseHelper getInstance(Context context) {
@@ -30,34 +31,47 @@ public class ApartmentDatabaseHelper extends SQLiteOpenHelper {
         return sInstance;
     }
 
+    private void setupDatabase() {
+        getReadableDatabase(); // creates an empty database file
+
+        try {
+            InputStream input = context.getAssets().open(DB_NAME + ".db");
+            OutputStream output = new FileOutputStream(context.getDatabasePath(DB_NAME));
+
+            //copy bytes from the input file to the output file
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = input.read(buffer)) > 0)
+                output.write(buffer, 0, length);
+
+            output.close();
+            input.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public void onCreate(SQLiteDatabase db) {
-        if (!context.getDatabasePath(DB_NAME).exists()) {
-            try {
-                InputStream input = context.getAssets().open(DB_NAME);
-                OutputStream output = new FileOutputStream(context.getApplicationInfo().dataDir + "/" + DB_NAME);
-
-                //copy bytes from the input file to the output file
-                byte[] buffer = new byte[1024];
-                int length;
-                while ((length = input.read(buffer)) > 0)
-                    output.write(buffer, 0, length);
-
-                output.close();
-                input.close();
-            } catch (IOException e) {
-                Log.d("Tom", "Database copy failed.");
-                e.printStackTrace();
-            }
-        }
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         if (oldVersion != newVersion) {
             context.deleteDatabase(DB_NAME);
-            this.onCreate(db);
+            setupDatabase();
         }
+    }
+
+    public static class Users {
+        public static final String TABLE_NAME = "users";
+        public static final String KEY_ID = "id";
+        public static final String KEY_EMAIL = "email";
+        public static final String KEY_PASSWORD = "password";
+        public static final String KEY_HINT = "hint";
+        public static final String KEY_NAME = "name";
+        public static final String KEY_ADDRESS = "address";
+        public static final String KEY_PHONE = "phone";
     }
 
 }
--- app/src/main/java/edu/iit/cs442/team15/ehome/LoginActivity.java
+++ app/src/main/java/edu/iit/cs442/team15/ehome/LoginActivity.java
@@ -1,17 +1,29 @@
 package edu.iit.cs442.team15.ehome;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import edu.iit.cs442.team15.ehome.ApartmentDatabaseHelper.Users;
+import edu.iit.cs442.team15.ehome.model.User;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText emailEditText;
+    EditText passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        emailEditText = (EditText) findViewById(R.id.emailEditText);
+        passwordEditText = (EditText) findViewById(R.id.passwordEditText);
 
         Button loginButton = (Button) findViewById(R.id.loginButton);
         loginButton.setOnClickListener(this);
@@ -24,15 +36,44 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.loginButton:
-                // TODO verify login info
-                Intent login = new Intent(this, MainActivity.class);
-                startActivity(login);
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                User user = login(email, password);
+
+                // TODO error feedback, pw hint, pass user data to MainActivity
+                if (user != null) {
+                    Intent login = new Intent(this, MainActivity.class);
+                    startActivity(login);
+                }
                 break;
             case R.id.registerButton:
                 Intent register = new Intent(this, CreateAccountActivity.class);
                 startActivity(register);
                 break;
         }
+    }
+
+    private User login(String email, String password) {
+        SQLiteDatabase db = ApartmentDatabaseHelper.getInstance(this).getReadableDatabase();
+
+        final String sqlQuery = "SELECT * FROM " + Users.TABLE_NAME + " WHERE " + Users.KEY_EMAIL + "=? AND " + Users.KEY_PASSWORD + "=?";
+        Cursor result = db.rawQuery(sqlQuery, new String[]{email, password});
+
+        User user = null;
+
+        if (result.moveToFirst()) {
+            // user exists
+            user = new User();
+            user.id = result.getInt(result.getColumnIndex(Users.KEY_ID));
+            user.passwordHint = result.getString(result.getColumnIndex(Users.KEY_HINT));
+            user.name = result.getString(result.getColumnIndex(Users.KEY_NAME));
+            user.address = result.getString(result.getColumnIndex(Users.KEY_ADDRESS));
+            user.phone = result.getString(result.getColumnIndex(Users.KEY_PHONE));
+        }
+        result.close();
+
+        return user;
     }
 
 }
