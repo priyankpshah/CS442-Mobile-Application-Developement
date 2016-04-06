54e5513dc9e793793817aa2ac0a80d52a900999e
Tom Molenhouse <tmolenho@hawk.iit.edu>
2016-04-06 18:49:56 -0500

    added login manager class

---
+++ app/src/main/java/edu/iit/cs442/team15/ehome/util/SavedLogin.java
@@ -0,0 +1,55 @@
+package edu.iit.cs442.team15.ehome.util;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.annotation.Nullable;
+
+import edu.iit.cs442.team15.ehome.util.ApartmentDatabaseHelper.Users;
+
+public final class SavedLogin {
+
+    public static final String SAVED_LOGIN_PREFS = "saved_login_prefs";
+    private static SavedLogin sInstance; // singleton instance
+
+    private SharedPreferences savedLoginPrefs;
+
+    private SavedLogin(Context context) {
+        savedLoginPrefs = context.getSharedPreferences(SAVED_LOGIN_PREFS, Context.MODE_PRIVATE);
+    }
+
+    public static synchronized void initialize(Context context) {
+        if (sInstance == null)
+            sInstance = new SavedLogin(context.getApplicationContext());
+    }
+
+    public static SavedLogin getInstance() {
+        if (sInstance == null)
+            throw new RuntimeException("SavedLogin not initialized.");
+        return sInstance;
+    }
+
+    @Nullable
+    public String getEmail() {
+        return savedLoginPrefs.getString(Users.KEY_EMAIL, null);
+    }
+
+    @Nullable
+    public String getPassword() {
+        return savedLoginPrefs.getString(Users.KEY_PASSWORD, null);
+    }
+
+    public void saveLogin(final String email, final String password) {
+        savedLoginPrefs.edit()
+                .putString(Users.KEY_EMAIL, email)
+                .putString(Users.KEY_PASSWORD, password)
+                .apply();
+    }
+
+    public void logout() {
+        savedLoginPrefs.edit()
+                .remove(Users.KEY_EMAIL)
+                .remove(Users.KEY_PASSWORD)
+                .apply();
+    }
+
+}
