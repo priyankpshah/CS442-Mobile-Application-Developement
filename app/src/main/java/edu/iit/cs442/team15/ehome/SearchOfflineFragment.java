fbfec06211f997633d827d59b2e4cf326c777d8e
Priyank <priyank687@gmail.com>
2016-04-07 20:55:17 -0500

    temp check

--- app/src/main/java/edu/iit/cs442/team15/ehome/SearchOfflineFragment.java
+++ app/src/main/java/edu/iit/cs442/team15/ehome/SearchOfflineFragment.java
@@ -1,5 +1,6 @@
 package edu.iit.cs442.team15.ehome;
 
+import android.app.ListFragment;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
@@ -9,14 +10,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link SearchOfflineFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link SearchOfflineFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SearchOfflineFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -30,18 +23,8 @@ public class SearchOfflineFragment extends Fragment {
     private OnFragmentInteractionListener mListener;
 
     public SearchOfflineFragment() {
-        // Required empty public constructor
-    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SearchOfflineFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+    }
     public static SearchOfflineFragment newInstance(String param1, String param2) {
         SearchOfflineFragment fragment = new SearchOfflineFragment();
         Bundle args = new Bundle();
@@ -63,7 +46,7 @@ public class SearchOfflineFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_search_offline, container, false);
     }
 
@@ -92,11 +75,7 @@ public class SearchOfflineFragment extends Fragment {
     }
 
     /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
+
      * See the Android Training lesson <a href=
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
