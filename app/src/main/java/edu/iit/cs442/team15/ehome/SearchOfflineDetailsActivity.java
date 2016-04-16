4ff471711c986ab799fe2fd7452838c21d347ccb
Tom <tmolenho@hawk.iit.edu>
2016-04-16 10:47:26 -0500

    improved code spelling & formatting

--- app/src/main/java/edu/iit/cs442/team15/ehome/SearchOfflineDetailsActivity.java
+++ app/src/main/java/edu/iit/cs442/team15/ehome/SearchOfflineDetailsActivity.java
@@ -19,9 +19,9 @@ import edu.iit.cs442.team15.ehome.util.ApartmentDatabaseHelper;
 public class SearchOfflineDetailsActivity extends AppCompatActivity {
     String value;
     FloatingActionButton call, text;
-    TextView address, Apartment,bdroom, bthroom,area,rent;
-    CheckedTextView cable,electricity,gas,gym,intenet,pspace,heat;
-    static ApartmentDatabaseHelper db;
+    TextView address, Apartment, bdroom, bthroom, area, rent;
+    CheckedTextView cable, electricity, gas, gym, intenet, pspace, heat;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,63 +30,50 @@ public class SearchOfflineDetailsActivity extends AppCompatActivity {
         Intent i = getIntent();
         value = i.getStringExtra("Position");
 
-        db = ApartmentDatabaseHelper.getInstance();
-        ArrayList<String> aptinfo = db.getDetails(value);
-        final ArrayList<String> ammenitiesinfo = db.getAmenities(value);
+        ApartmentDatabaseHelper adh = ApartmentDatabaseHelper.getInstance();
+        ArrayList<String> apartments = adh.getDetails(value);
+        final ArrayList<String> amenities = adh.getAmenities(value);
 
-        address = (TextView)findViewById(R.id.Address);
-        Apartment = (TextView)findViewById(R.id.Apartment_name);
-        area= (TextView)findViewById(R.id.squarefoot);
-        bthroom= (TextView)findViewById(R.id.Bathroom);
-        bdroom= (TextView)findViewById(R.id.Bedroom);
-        rent = (TextView)findViewById(R.id.rent);
-        address.setText("Address: "+aptinfo.get(0));
-        area.setText("Area: "+aptinfo.get(1)+"sq. ft.");
-        bdroom.setText("Bedrooms: "+aptinfo.get(2));
-        bthroom.setText("Bathrooms: "+aptinfo.get(3));
-        rent.setText("Rent: $"+aptinfo.get(4));
+        address = (TextView) findViewById(R.id.Address);
+        Apartment = (TextView) findViewById(R.id.Apartment_name);
+        area = (TextView) findViewById(R.id.squarefoot);
+        bthroom = (TextView) findViewById(R.id.Bathroom);
+        bdroom = (TextView) findViewById(R.id.Bedroom);
+        rent = (TextView) findViewById(R.id.rent);
+        address.setText("Address: " + apartments.get(0));
+        area.setText("Area: " + apartments.get(1) + "sq. ft.");
+        bdroom.setText("Bedrooms: " + apartments.get(2));
+        bthroom.setText("Bathrooms: " + apartments.get(3));
+        rent.setText("Rent: $" + apartments.get(4));
 
-        pspace= (CheckedTextView)findViewById(R.id.parking);
-        heat= (CheckedTextView)findViewById(R.id.thermostate);
-        gas= (CheckedTextView)findViewById(R.id.gas);
-        gym= (CheckedTextView)findViewById(R.id.gym);
-        electricity = (CheckedTextView)findViewById(R.id.Electricity);
-        //cable = (CheckedTextView)findViewById(R.id.cable);0
-        //internet = (CheckedTextView)findViewById(R.id.internet);4
+        pspace = (CheckedTextView) findViewById(R.id.parking);
+        heat = (CheckedTextView) findViewById(R.id.thermostate);
+        gas = (CheckedTextView) findViewById(R.id.gas);
+        gym = (CheckedTextView) findViewById(R.id.gym);
+        electricity = (CheckedTextView) findViewById(R.id.Electricity);
+        //cable = (CheckedTextView) findViewById(R.id.cable); //0
+        //internet = (CheckedTextView) findViewById(R.id.internet); //4
 
-//        if(ammenitiesinfo.get(0)=="1")
-//        {
-//            cable.setChecked(true);
-//        }
-        String flag1 = "1";
-        if(String.valueOf(ammenitiesinfo.get(1))== flag1)
-        {
+        //if (ammenitiesinfo.get(0) == "1")
+        //    cable.setChecked(true);
+
+        if (String.valueOf(amenities.get(1)) == "1")
             electricity.setChecked(true);
-        }
-        if(ammenitiesinfo.get(2)=="1")
-        {
+
+        if (amenities.get(2) == "1")
             gas.setChecked(true);
-        }
-        if(ammenitiesinfo.get(3)=="TRUE")
-        {
+
+        if (amenities.get(3) == "TRUE")
             gym.setChecked(true);
-        }
-//        if(ammenitiesinfo.get(4)=="1")
-//        {
-//            intenet.setChecked(true);
-//        }
 
-        if(ammenitiesinfo.get(5)=="TRUE")
-        {
+        if (amenities.get(4) == "1")
+            intenet.setChecked(true);
+
+        if (amenities.get(5) == "TRUE")
             pspace.setChecked(true);
-        }
-        if(ammenitiesinfo.get(6)=="1")
-        {
-            heat.setChecked(true);
-        }
 
-
-
+        //if (ammenitiesinfo.get(6) == "1")
+        //    heat.setChecked(true);
 
         //Call Owner with the following method
         call = (FloatingActionButton) findViewById(R.id.Call);
@@ -95,19 +82,20 @@ public class SearchOfflineDetailsActivity extends AppCompatActivity {
                 Intent makeCall = new Intent(Intent.ACTION_CALL);
                 String phNum = "tel:" + "3126473207";
                 makeCall.setData(Uri.parse(phNum));
-                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CALL_PHONE) ==
-                        PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
                     startActivity(makeCall);
-                } }
+                }
+            }
         });
+
         //Send Message to Owner
         text = (FloatingActionButton) findViewById(R.id.sms);
         text.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent sendText = new Intent(Intent.ACTION_VIEW);
-                sendText.putExtra("address", new String("3126473207"));
-                sendText.putExtra("sms_body","I am Interested in leasing you property, Contact me!!");
+                sendText.putExtra("address", "3126473207");
+                sendText.putExtra("sms_body", "I am Interested in leasing you property, Contact me!!");
             }
         });
 
